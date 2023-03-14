3aa19f22cf11bd60218fc6ed7b889c1678397b75
Mohamed-aly77 <125808033+Mohamed-aly77@users.noreply.github.com>
2023-03-14 11:59:48 +0200

    final refractor

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -16,11 +16,16 @@ function Testimonials() {
           What people are <br className=" sm:block hidden" /> saying about us
         </h1>
         <div className=" w-full md:mt-0 mt-6">
-          <p>
+          <p className={`${styles.paragraph} text-left max-w-[450px]`}>
             Everything you need to accept card payments and grow your business
             anywhere on the planet.
           </p>
         </div>
+      </div>
+      <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-container relative z-[1]">
+        {feedback.map((card) => (
+          <FeedBack key={card.id} {...card} />
+        ))}
       </div>
     </section>
   );
